35a2ed57286b0366f747c6fd1cfb123773824b5d
Masedie <masedie@Masedies-MacBook-Air.local>
2024-10-30 00:18:28 +0200

    Adding New Comments to login.js

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
-
+// Import the functions needed from the SDKs.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
-
+// The configuration for my app
 const firebaseConfig = {
     apiKey: "AIzaSyDHNWOW1nE29kaBkERIGRUjovvTWUiqHl8",
     authDomain: "simplified-netflix-clone.firebaseapp.com",
@@ -13,28 +13,28 @@ const firebaseConfig = {
     measurementId: "G-J5T2ZT47R1",
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
 
-
+// Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', () => {
-    
+    // Add an event listener to the login form
     document.getElementById('login-form').addEventListener('submit', function (e) {
         e.preventDefault();
 
-        
+        // Get the email and password values
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        
+        // Sign in the user with the email and password
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
             
                 window.location.href = 'index.html';
             })
             .catch((error) => {
-            
+            // Handle errors
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 document.getElementById('error-message').innerText = errorMessage; // Display error to the user
